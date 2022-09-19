debab417c4b9505604a7c7974c6124c42b7af4ef
lsx-ctfliar <lsx-ctfliar@outlook.com>
2022-09-19 14:53:57 +0800

    lsx网络配置

---
+++ app/src/main/java/com/example/login_in_app/RegisterActivity.java
@@ -0,0 +1,33 @@
+package com.example.login_in_app;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.NetworkOnMainThreadException;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class RegisterActivity {
+
+
+
+
+
+}
