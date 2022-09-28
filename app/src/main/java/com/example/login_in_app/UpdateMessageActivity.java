0787f5c01081ea755c3183914ee08e5ddfe364ca
lsx-ctfliar <lsx-ctfliar@outlook.com>
2022-09-28 10:10:45 +0800

    修改实现

--- app/src/main/java/com/example/login_in_app/UpdateMessageActivity.java
+++ app/src/main/java/com/example/login_in_app/UpdateMessageActivity.java
@@ -3,6 +3,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.NetworkOnMainThreadException;
@@ -37,7 +38,8 @@ public class UpdateMessageActivity extends AppCompatActivity {
 
     private final Gson gson = new Gson();
     private AlertDialog dlg;
-
+    TextView update_account;
+    ImageView update_btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,34 +51,66 @@ public class UpdateMessageActivity extends AppCompatActivity {
 
         //设置图片编辑
 
-        TextView account=findViewById(R.id.UpdateUser);
+        update_account=findViewById(R.id.UpdateUser);
         //设置点击按钮，编辑昵称信息页面
-       ImageView btn1=findViewById(R.id.btn1);
-       btn1.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
+       update_btn1=findViewById(R.id.btn1);
+        Listener listener=new Listener();
+        update_btn1.setOnClickListener(listener);
+
+
+
+        }
+    class Listener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            System.out.println("R.id的值是："+view.getId());
+            switch(view.getId()){
+                case R.id.btn1:
+//                    System.out.println("点击事件没有问题");
+//                    View cut_view = LayoutInflater.from(UpdateMessageActivity.this).inflate(R.layout.activity_change_one_message, null);
+//                    System.out.println("打开编辑界面没问题");
+//                    EditText editor = cut_view.findViewById(R.id.update_account);
+//                    Button bt_eridotr = cut_view.findViewById(R.id.update_save);
+//                    bt_eridotr.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            update_account.setText(editor.getText().toString());
+//                            System.out.println(update_account.getText().toString());
+//                            dlg.dismiss();
+//                        }
+//                    });
+//                    dlg = new AlertDialog.Builder(UpdateMessageActivity.this)
+//                            .setView(cut_view)
+//                            .create();
+//                    dlg.show();
+                    final EditText inputServer = new EditText(UpdateMessageActivity.this);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(UpdateMessageActivity.this);
+                    builder.setTitle("修改昵称").setIcon(android.R.drawable.ic_dialog_info).setView(inputServer)
+                            .setNegativeButton("Cancel", null);
+                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+                                public void onClick(DialogInterface dialog, int which) {
+                                    update_account.setText(inputServer.getText().toString());
+
+                                }});
+
+                    builder.show();
+
+                    break;
+
+            }
+        }
+
 //               Intent intent = new Intent(UpdateMessageActivity.this,ChangeOneMessageActivity.class);
 //               startActivity(intent);
 //               要在同一个activity下面布局`
-               View cut_view = LayoutInflater.from(UpdateMessageActivity.this).inflate(R.layout.activity_change_one_message,null);
-               EditText editor = cut_view.findViewById(R.id.update_account);
-               Button bt_eridotr = cut_view.findViewById(R.id.update_save);
-               bt_eridotr.setOnClickListener(new View.OnClickListener() {
-                   @Override
-                   public void onClick(View v) {
-                       account.setText(editor.getText().toString());
-                       dlg.dismiss();
-                   }
-               });
-               dlg= new AlertDialog.Builder(UpdateMessageActivity.this)
-                       .setView(cut_view)
-                       .create();
-               dlg.show();
 
 
-           }
-       });
+
+
+
+    }
     }
 
 
-}
+
